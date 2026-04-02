0bebd6c07f3a9e25393909c50b4da7146b19153a
bfrasholb <13065@holbertonstudents.com>
2026-04-02 14:38:52 +1100

    Allthethingsthatyouowninaboxtotheright

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right- inserts a node in parent->right
+ * @parent: the parent node
+ * @value: value to store
+ * Return: returns a pointer to the new node
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *insert;
+
+	if (!parent)
+		return (NULL);
+	insert = malloc(sizeof(*insert));
+	if (!insert)
+		return (NULL);
+	insert->n = value;
+	insert->parent = parent;
+	insert->left = NULL;
+	insert->right = parent->right;
+	parent->left = insert;
+	if (insert->left)
+		insert->left->parent = insert;
+	return (insert);
+}
