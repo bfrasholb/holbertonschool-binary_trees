81af5cd48736ebbf20cc9d549d7186217bb4becc
bfrasholb <13065@holbertonstudents.com>
2026-04-02 21:49:21 +1100

    4 lines not bad

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,8 +10,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	int isleaf = 0;
 
-	if (!node)
-		return (isleaf);
 	if (!(node->left) && !(node->right))
 		isleaf = 1;
 	return (isleaf);
---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root- checks if a given node is a root
+ * @node: the node to check
+ * Return: returns 1 if node is a root, else 0
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	int isroot = 0;
+
+	if (!(node->parent))
+		isroot = 1;
+	return (isroot);
+}
+
