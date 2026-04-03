668068f92687767bf3d70ee7abb403fcbf5a6514
bfrasholb <13065@holbertonstudents.com>
2026-04-03 12:50:36 +1100

    Formatting

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,7 +6,16 @@
  * Return: the depth of tree, or 0 if !tree
  */
 
- size_t binary_tree_depth(const binary_tree_t *tree)
- {
-	
- }
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t height;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->parent == NULL)
+		return (0);
+
+	height = binary_tree_depth(tree->parent);
+	return (height + 1);
+}
