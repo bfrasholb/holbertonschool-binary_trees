c4a1e9a266a4f75b341040be99205be32d3dba7b
bfrasholb <13065@holbertonstudents.com>
2026-04-03 14:14:10 +1100

    Full? I'm hungry

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full- checks if a tree is full
+ * @tree: a pointer to the root node
+ * Return: 1 if full, else 0
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->right && tree->left)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);	
+}
