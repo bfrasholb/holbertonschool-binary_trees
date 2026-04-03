492ae72cf1253804b29f1fdc960cac4cb10ea8a3
bfrasholb <13065@holbertonstudents.com>
2026-04-03 13:59:02 +1100

    Formatting

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,11 +13,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+	if (tree->left == NULL)
+		lheight = (size_t)-1;
+	else
+		lheight = binary_tree_height(tree->left);
 
-	lheight = binary_tree_height(tree->left);
-	rheight = binary_tree_height(tree->right);
+	if (tree->right == NULL)
+		rheight = (size_t)-1;
+	else
+		rheight = binary_tree_height(tree->right);
 
 	if (lheight > rheight)
 		return (lheight + 1);
