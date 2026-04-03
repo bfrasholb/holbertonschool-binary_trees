b3ebf1036fb6456a97abaeea011d62649aafac6f
bfrasholb <13065@holbertonstudents.com>
2026-04-03 17:08:05 +1100

    Tidying files

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,27 @@
 #include "binary_trees.h"
-#include "14-bintree_height.c"
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lheight, rheight;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL)
+		lheight = 0;
+	else
+		lheight = binary_tree_height(tree->left);
+
+	if (tree->right == NULL)
+		rheight = 0;
+	else
+		rheight = binary_tree_height(tree->right);
+
+	if (lheight > rheight)
+		return (lheight + 1);
+	else
+		return (rheight + 1);
+}
 
 /**
  * binary_tree_balance- measures the balance factor of a binary tree
