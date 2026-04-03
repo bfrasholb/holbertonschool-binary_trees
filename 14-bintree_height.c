fabd265f12a614de89201139d2c2aa04f571ee3b
bfrasholb <13065@holbertonstudents.com>
2026-04-03 14:07:35 +1100

    Formatting

---
+++ 14-bintree_height.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height- function finds the height of a binary tree
+ * @tree: a pointer to the root node
+ * Return: 0 if tree == NULL, else tree height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lheight, rheight;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL)
+		lheight = 0;
+	else
+		lheight = binary_tree_height(tree->left);
+
+	if (tree->right == NULL)
+		rheight = 0;
+	else
+		rheight = binary_tree_height(tree->right);
+
+	if (lheight > rheight)
+		return (lheight + 1);
+	else
+		return (rheight + 1);
+}
